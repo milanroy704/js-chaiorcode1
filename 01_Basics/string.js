94cfe1a5b84637e626b90d1c2cf4dd80a0652d07
Milan Raj <98047727+milanroy704@users.noreply.github.com>
2024-01-15 18:31:05 +0000

    string

---
+++ 01_Basics/string.js
@@ -0,0 +1,24 @@
+let myString= "Milan "
+let age= 23
+
+console.log(myString + age + " Hai");
+console.log(`Hello My Name is ${myString} and My age is ${age}`);
+let string = new String("Website Development")
+
+// console.log(string.toUpperCase())
+// console.log(string[1]);
+// console.log(string.__proto__);
+// console.log(string.charAt[3]);
+// console.log(string.indexOf('v'));
+
+// let newString = string.substring(4,10);  //return part of string except last Number that is 10 from 4th index to last 9 index 
+// console.log(newString);
+
+// let partString= string.slice(3,12)
+// console.log(partString);
+
+let mypower= "   NishaSingh   "
+console.log(mypower);
+
+console.log(mypower.trim());
+console.log(mypower.length);
