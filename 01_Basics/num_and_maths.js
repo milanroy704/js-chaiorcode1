6b4adfb729b210f752fa0fd27d41ae7b28775f9a
Milan Raj <98047727+milanroy704@users.noreply.github.com>
2024-01-15 18:57:49 +0000

    num and math

---
+++ 01_Basics/num_and_maths.js
@@ -0,0 +1,33 @@
+const number= 400;
+// console.log(number);
+
+ const score= new Number(400);
+//  console.log(score);
+
+//  console.log(score.toString().length);
+//  console.log(score.toFixed(1));
+
+ const decimalNo= 243.34;
+//  console.log(decimalNo.toPrecision(3));
+
+ const convert= 1004320;
+//  console.log(convert.toLocaleString('en-IN'));
+
+ // ++++++++++++++       Maths     ++++++++++++++++++++++++
+
+//  console.log(Math);
+//  console.log(Math.abs(-3));
+//  console.log(Math.round(4.3));
+//  console.log(Math.floor(5.90));
+//  console.log(Math.ceil(5.2));
+//  console.log(Math.min(4,6,2,9,2,8));
+//  console.log(Math.max(4,6,2,9,7,8));
+
+console.log(Math.random);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const max=20;
+const min= 80;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
